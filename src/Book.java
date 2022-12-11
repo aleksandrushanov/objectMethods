af17aec1eb04843c1996b2dbb40317b6145727fa
Саша <kresh_sasha@icloud.com>
2022-12-11 16:07:06 +0700

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,21 @@
+public class Book {
+    private String name;
+    private Author authorName;
+    private int yearOfPublication;
+
+    public Book(String name, Author authorName, int yearOfPublication) {
+        this.name = name;
+        this.authorName = authorName;
+        this.yearOfPublication = yearOfPublication;
+    }
+    public String getName(){
+        return name;
+    }
+    public Author getAuthorName() {
+        return authorName;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            Author author = new Author("Рональд", "Толкин");
+            Author author1 = new Author("Джоан", "Роулинг");
+            Book book = new Book("Властелин коллец Браство кольца", author, 1941);
+            Book book1 = new Book("Гарри Поттер и филосовский камень", author1, 1997);
+            System.out.println(author.getFirstName() + " " +author.getLastName() + " " + book.getName() + " " + book.getYearOfPublication());
+            System.out.println(author1.getFirstName() + " " + author1.getLastName() + " " + book1.getName() + " " + book1.getYearOfPublication());
+        }
+    }
+}
